bbf7eab85f86fc55013e0874c299dd8a5d22ae04
johnvo <vojohn95@gmail.com>
2023-01-07 02:10:54 -0600

    first commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    //print two vars
+    // let edad: u8 = 25;
+    // let nombre: &str = "Omar";
+    // println!("Soy {nombre}, tengo {edad} años");
+
+    //get user inputs and print
+    // let mut edad = String::new();
+    // let mut nombre = String::new();
+    // println!("Ingresa tu edad");
+    // std::io::stdin().read_line(&mut edad).expect("Error");
+    // println!("Ingresa tu nombre");
+    // std::io::stdin().read_line(&mut nombre).expect("Error");
+    // println!("Hola {}, tienes {} años", nombre, edad);
+    
+}
