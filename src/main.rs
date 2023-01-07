1548299818357ac6a7e751e08997140e9d4a1c92
johnvo <vojohn95@gmail.com>
2023-01-07 02:46:10 -0600

    conditions

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,62 @@
-fn main() {
-    //print two vars
-    // let edad: u8 = 25;
-    // let nombre: &str = "Omar";
-    // println!("Soy {nombre}, tengo {edad} años");
+use std::io;
 
-    //get user inputs and print
-    // let mut edad = String::new();
-    // let mut nombre = String::new();
-    // println!("Ingresa tu edad");
-    // std::io::stdin().read_line(&mut edad).expect("Error");
-    // println!("Ingresa tu nombre");
-    // std::io::stdin().read_line(&mut nombre).expect("Error");
-    // println!("Hola {}, tienes {} años", nombre, edad);
+fn main() {
+    /* Exercise one: Print vars */
+    //print_vars();
     
+    /* Exercise two  Receive inputs*/
+    //get_user_input();
+
+    // /* Exercise tree: conditions */
+    conditions_print();
+
+}
+
+fn print_vars() {
+    //print two vars
+    let edad: u8 = 25;
+    let nombre: &str = "John";
+    println!("Soy {nombre}, tengo {edad} años");
+}
+
+fn get_user_input(){
+    //get user inputs and print
+    let mut edad = String::new();
+    let mut nombre = String::new();
+
+    println!("Ingresa tu edad");
+    //get user input
+    io::stdin().read_line(&mut edad).expect("Error");
+    //convert string to number
+    let edad_int: u8 = edad.trim().parse().expect("Error");
+    
+    println!("Ingresa tu nombre");
+    io::stdin().read_line(&mut nombre).expect("Error");
+    //Create var with user inputs
+    //let mut frase: String = String::new();
+    let frase = format!("Hola {}, tienes {} años", nombre, edad_int);
+    println!("{}", frase);
+}
+
+fn conditions_print() {
+    //get user inputs and print
+    let mut edad = String::new();
+    let mut nombre = String::new();
+
+    println!("Ingresa tu edad");
+    io::stdin().read_line(&mut edad).expect("Error");
+    //convert string to number
+    let edad_int: u8 = edad.trim().parse().expect("Error");
+    
+    println!("Ingresa tu nombre");
+    io::stdin().read_line(&mut nombre).expect("Error");
+    //Create var with user inputs
+    //let mut frase: String = String::new();
+    let frase = format!("Hola {}, tienes {} años", nombre, edad_int);
+    if edad_int >= 18 {
+        //print string with vars
+        println!("{}, Eres mayor de edad", frase);
+    } else {
+        println!("{}, Eres menor de edad", frase);
+    }
 }
